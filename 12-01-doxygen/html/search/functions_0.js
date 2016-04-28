f7d28383a6cb164fab34d5736309b9a6f77d6221
philipijnos <philie753@gmail.com>
2016-04-28 14:15:24 +0200

    Doxygen opdracht uitgevoerd

---
+++ 12-01-doxygen/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['operator_2a',['operator*',['../classvector.html#af5eb8137df41617181487e49d85df888',1,'vector']]]
+];
--- 12-01-doxygen/main.cpp
+++ 12-01-doxygen/main.cpp
@@ -1,5 +1,11 @@
+/// @file
+
 #include "vector.hpp"
 
+/// Absolutely nothing
+//
+/// This file does absolutely nothing and therefore is completely useless.
+/// Would recommend to to put actually working stuff in here.
 int main(int argc, char **argv){
    return 0;
 }
