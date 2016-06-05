5ef1516bd7317b853f25a734b2e31355e16b9bd5
philipijnos <philie753@gmail.com>
2016-06-05 23:13:21 +0200

    22.2 af

--- opdracht22_2/main.cpp
+++ opdracht22_2/main.cpp
@@ -1,18 +1,5 @@
 #include "hwlib.hpp"
 
-void kitt( hwlib::port_out & leds, int ms = 80 ){
-   for(;;){
-      for( unsigned int i = 0; i < leds.number_of_pins(); ++i ){
-         leds.set( 0x01 << i );      
-         hwlib::wait_ms( ms );
-      }
-      for( unsigned int i = leds.number_of_pins() - 2; i > 0; --i ){
-         leds.set( 0x01 << i );      
-         hwlib::wait_ms( ms );
-      }
-   }
-}
-
 int main( void ){	
    // kill the watchdog
    WDT->WDT_MR = WDT_MR_WDDIS;
@@ -22,6 +9,10 @@ int main( void ){
    auto led2 = hwlib::target::pin_out( hwlib::target::pins::d5 );
    auto led3 = hwlib::target::pin_out( hwlib::target::pins::d4 );
    
-   auto leds = hwlib::port_out_from_pins( led0, led1, led2, led3 );
-   ::kitt( leds );
+   while(true){
+	   led3.set(0); led1.set(1); hwlib::wait_ms(300);
+	   led0.set(0); led2.set(1); hwlib::wait_ms(300);
+	   led1.set(0); led3.set(1); hwlib::wait_ms(300);
+	   led2.set(0); led0.set(1); hwlib::wait_ms(300);
+   }
 }
